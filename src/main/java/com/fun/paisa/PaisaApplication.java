eda785ef87c5028058ebe4843f157f0fb73a84e3
rakesh.kumar <kota.kumar@infoedge.com>
2020-06-07 10:51:09 +0530

    Spring init

---
+++ src/main/java/com/fun/paisa/PaisaApplication.java
@@ -0,0 +1,13 @@
+package com.fun.paisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fun/paisa/PaisaVasoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fun.paisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisaVasoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
